ddf06f48da0a78befc706b3f3289ef0956a031cb
Korneliusz Caputa <helluinster@gmail.com>
2014-11-15 12:54:49 +0100

    main/main.go

--- main/main.go
+++ main/main.go
@@ -1,24 +1,65 @@
 package main
 
-type Cell struct {
-	X, Y int
-}
+type Cell bool
 
-type Board [][]*Cell
+type Board [][]Cell
 
-func MakeBoard(seed [][]int) *Board {
+func MakeBoard(seed [][]int) Board {
 	result := make(Board, len(seed))
 	for i := 0; i < len(seed); i++ {
-		result[i] = make([]*Cell, len(seed[i]))
+		result[i] = make([]Cell, len(seed[i]))
 		for j := 0; j < len(seed[i]); j++ {
 			if seed[i][j] == 1 {
-				result[i][j] = &Cell{
-					X: i,
-					Y: j,
-				}
+				result[i][j] = Cell(true)
 			}
 
 		}
 	}
-	return &result
+	return result
+}
+
+func Next(input Board) (result Board) {
+	result = make(Board, len(input))
+	for i := 0; i < len(input); i++ {
+		copy(result[i], input[i])
+	}
+
+	return input
+}
+
+func (self Board) countAliveNeighbors(x, y int) int {
+	alive := 0
+	for xi := -1; xi <= 1; xi++ {
+		theX := x + xi
+		if theX < 0 || theX >= len(self[theX]) {
+			continue
+		}
+
+		for yi := -1; yi <= 1; yi++ {
+			theY := y + yi
+			if theY < 0 || theY >= len(self[theX]) {
+				continue
+			}
+
+			if yi == 0 && xi == 0 {
+				continue
+			}
+
+			if self[theX][theY] {
+				alive++
+			}
+		}
+	}
+
+	return alive
+}
+
+func (self Board) ShouldDie(x, y int) bool {
+	aliveNeighbors := self.countAliveNeighbors(x, y)
+	if self[x][y] { // self is alive
+		return aliveNeighbors < 2 || aliveNeighbors > 3
+	} else {
+		return false
+	}
+
 }
--- main/main_test.go
+++ main/main_test.go
@@ -4,44 +4,88 @@ import "testing"
 import "github.com/stretchr/testify/assert"
 import "github.com/elkorn/code_retreat-game_of_life/main"
 
+var seed [][]int = [][]int{
+	[]int{0, 1, 0},
+	[]int{0, 1, 1},
+	[]int{1, 1, 0},
+}
 
 func TestMakeCell(t *testing.T) {
-    c := &main.Cell{
-        X: 0,
-        Y: 0,
-    }
+	c := main.Cell(true)
 
-    assert.NotNil(t, c)
-    assert.Equal(t, 0, c.X)
-    assert.Equal(t, 0, c.Y)
+	assert.True(t, bool(c))
 }
 
 func TestHaveBoard(t *testing.T) {
-    seed := [][] int {
-        []int{0, 1, 0},
-        []int{0, 1, 1},
-        []int{1, 1, 0},
-    }
-
-    board := main.MakeBoard(seed)
-    assert.NotNil(t, board)
-    assert.Equal(t, len(seed), len(*board))
-    for i := 0; i < len(*board); i++ {
-        assert.Equal(t, len(seed[i]), len((*board)[i]))
-    }
-
+	board := main.MakeBoard(seed)
+	assert.NotNil(t, board)
+	assert.Equal(t, len(seed), len(board))
+	for i := 0; i < len(board); i++ {
+		assert.Equal(t, len(seed[i]), len(board[i]))
+	}
 }
 
 func TestBoardSeed(t *testing.T) {
-    seed := [][] int {
-        []int{0, 1, 0},
-        []int{0, 1, 1},
-        []int{1, 1, 0},
-    }
+	board := main.MakeBoard(seed)
 
-    board := main.MakeBoard(seed)
+	assert.True(t, bool(board[0][1]))
+	assert.True(t, bool(board[1][2]))
+	assert.True(t, bool(board[2][0]))
+}
 
-    assert.NotNil(t, (*board)[0][1])
-    assert.NotNil(t, (*board)[1][2])
-    assert.NotNil(t, (*board)[2][0])
+func TestNext(t *testing.T) {
+	board1 := main.MakeBoard(seed)
+	next := main.Next(board1)
+	assert.NotNil(t, next)
+	assert.Equal(t, len(board1), len(next))
+	for i := 0; i < len(next); i++ {
+		assert.Equal(t, len(board1[i]), len(next[i]))
+	}
+}
+
+func defaultBoard() (board main.Board) {
+	board = main.MakeBoard(seed)
+	return
+}
+
+func TestShouldDieWhenLessThan2(t *testing.T) {
+	seed := [][]int{
+		[]int{0, 0, 0},
+		[]int{0, 1, 1},
+		[]int{1, 1, 0},
+	}
+
+	board := main.MakeBoard(seed)
+	assert.True(t, board.ShouldDie(0, 0))
+	seed = [][]int{
+		[]int{0, 1, 0},
+		[]int{0, 1, 1},
+		[]int{1, 1, 0},
+	}
+
+	board = main.MakeBoard(seed)
+	assert.False(t, board.ShouldDie(0, 0))
+}
+
+func TestShouldDieWhenMoreThan3(t *testing.T) {
+	seed := [][]int{
+		[]int{1, 1, 1},
+		[]int{1, 1, 1},
+		[]int{1, 1, 1},
+	}
+
+	board := main.MakeBoard(seed)
+	assert.True(t, board.ShouldDie(0, 1))
+}
+
+func TestShouldLiveWhenLiveAnd2Or3(t *testing.T) {
+	seed := [][]int{
+		[]int{1, 1, 0},
+		[]int{0, 1, 1},
+		[]int{0, 1, 0},
+	}
+
+	board := main.MakeBoard(seed)
+	assert.False(t, board.ShouldDie(0, 0))
+	assert.False(t, board.ShouldDie(1, 2))
 }
